c31382a771239d52c795ce623a3c822d5cfa7397
Alexander Thomas <6ftDwarf@gmail.com>
2018-10-31 08:57:24 -0700

    Add basic octasphere scripts & gameobject

---
+++ Assets/Scripts/Octasphere/OctasphereCreator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class OctasphereCreator {
+
+	public static Mesh Create(int subdivisions, float radius)
+    {
+        Vector3[] verts =
+        {
+            Vector3.down,
+            Vector3.forward,
+            Vector3.left,
+            Vector3.back,
+            Vector3.right,
+            Vector3.up
+        };
+
+        int[] tris =
+        {
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4,
+            0, 4, 1,
+
+            5, 2, 1,
+            5, 3, 2,
+            5, 4, 3,
+            5, 1, 4
+        };
+
+        if (radius != 1f)
+        {
+            for(int i = 0; i < verts.Length; ++i)
+            {
+                verts[i] *= radius;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.name = "Octasphere";
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        return mesh;
+    }
+
+}
---
+++ Assets/Scripts/Octasphere/OctasphereTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class OctasphereTester : MonoBehaviour {
+
+    [SerializeField]
+    private int subdivisions = 0;
+    [SerializeField]
+    private float radius = 1f;
+    [SerializeField]
+    private MeshFilter meshFilter;
+
+
+
+    private void Awake()
+    {
+        meshFilter.mesh = OctasphereCreator.Create(subdivisions, radius);
+    }
+
+}
