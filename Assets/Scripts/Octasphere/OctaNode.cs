0dbbc72749ab1aac7694679e1256018e012d7d88
Alexander Thomas <6ftDwarf@gmail.com>
2018-11-03 17:51:43 -0700

    Begin work on OctaNode

---
+++ Assets/Scripts/Octasphere/OctaNode.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class OctaNode : MonoBehaviour {
+
+    [SerializeField]
+    private MeshFilter meshFilter;
+    [SerializeField]
+    private MeshRenderer meshRenderer;
+
+
+
+	public void Build(Vector3[] corners, int divisions)
+    {
+        // rez is the number of vertices on one side of the mesh/triangle
+        // the part in parentheses is called the "Mersenne Number"
+        int rez = 2 + ((int)Mathf.Pow(2, divisions) - 1);
+        // nTris is the number of tris in the mesh
+        int t = rez - 2;
+        int nTris = (t * (t + 1)) + (rez - 1);
+        // nVerts is the number of vertices in the mesh
+        // it is the formula for the "Triangle Sequence" of numbers
+        int nVerts = (rez * (rez + 1)) / 2;
+
+        Vector3[] vertices = new Vector3[nVerts];
+        int[] indices = new int[nTris * 3];
+
+        float dist01 = Vector3.Distance(corners[0], corners[1]);
+        float dist12 = Vector3.Distance(corners[1], corners[2]);
+        float dist20 = Vector3.Distance(corners[2], corners[0]);
+
+        float lenAxis01 = dist01 / (rez - 1);
+        float lenAxis12 = dist12 / (rez - 1);
+        float lenAxis20 = dist20 / (rez - 1);
+
+        Vector3 add1 = (corners[1] - corners[0]).normalized * lenAxis01;
+        Vector3 add2 = (corners[2] - corners[1]).normalized * lenAxis12;
+
+        int vIdx = 0;
+
+        for(int i = 0; i < rez; ++i)
+        {
+            for(int n = 0; n <= i; ++n)
+            {
+                vertices[vIdx] = corners[0] + add1 * i + add2 * n;
+                ++vIdx;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        meshFilter.mesh = mesh;
+    }
+
+}
---
+++ Assets/Scripts/Octasphere/OctaNodeTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OctaNode))]
+public class OctaNodeTester : MonoBehaviour {
+
+    [SerializeField]
+    private OctaNode node;
+    [SerializeField]
+    private int divisions;
+    [SerializeField]
+    private Vector3[] corners;
+
+
+
+    private void OnValidate()
+    {
+        if (corners.Length != 3)
+            corners = new Vector3[3];
+    }
+
+    private void Start()
+    {
+        node.Build(corners, divisions);
+    }
+
+}
