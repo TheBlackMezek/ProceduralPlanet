59c42be74902c5b079bc74295c03bf96753cfab7
Alexander Thomas <6ftDwarf@gmail.com>
2018-11-06 11:56:21 -0800

    Add showcase scene

---
+++ Assets/Scripts/SimpleOrbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleOrbit : MonoBehaviour {
+
+    [SerializeField]
+    private Transform parent;
+    [SerializeField]
+    private Vector3 rotationAxis;
+    [SerializeField]
+    private float rotationRate;
+
+
+
+    private void Update()
+    {
+        transform.RotateAround(parent.transform.position, rotationAxis, rotationRate * Time.deltaTime);
+    }
+
+}
